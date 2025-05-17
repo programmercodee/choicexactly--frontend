d74e085fb6779bdf48f9572dfd82365e5f9c2b96
Brijesh <programmercode102@gmail.com>
2025-05-18 00:45:22 +0530

    first commit

--- src/components/shopping-view/product-details.jsx
+++ src/components/shopping-view/product-details.jsx
@@ -106,7 +106,7 @@ function ProductDetailsDialog({ open, setOpen, productDetails }) {
 
   return (
     <Dialog open={open} onOpenChange={handleDialogClose}>
-      <DialogContent className="grid grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:max-w-[70vw]">
+      <DialogContent className="grid md:grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:max-w-[70vw]">
         <div className="relative overflow-hidden rounded-lg">
           <img
             src={productDetails?.image}
